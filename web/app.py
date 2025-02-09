40e44b97faf71db6ccb01a969d6ed2b07c28812d
inakivelasco <ivr.inakivelasco@gmail.com>
2025-02-09 20:34:37 +0100

    Add legal note

--- web/app.py
+++ web/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 import mysql.connector
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 def get_db_connection():
     return mysql.connector.connect(
@@ -11,15 +11,23 @@ def get_db_connection():
         database='weather_db'
         )
 
-@app.route("/")
-def hello_world():
-    connection = get_db_connection()
-    cursor =  connection.cursor()
+# @app.route("/")
+# def hello_world():
+#     connection = get_db_connection()
+#     cursor =  connection.cursor()
 
-    cursor.execute("SELECT * FROM weather")
-    result = cursor.fetchall()
-    connection.close()
-    return str(result)
+#     cursor.execute("SELECT * FROM weather")
+#     result = cursor.fetchall()
+#     connection.close()
+#     return str(result)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/legal_note")
+def legal_note():
+    return render_template("legal_note.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
