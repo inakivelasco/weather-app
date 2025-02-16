5b8a0826a6e024ba078312e488b75c70b7348040
inakivelasco <ivr.inakivelasco@gmail.com>
2025-02-16 19:53:51 +0100

    Improved visualization to make it interactive

--- web/app.py
+++ web/app.py
@@ -1,31 +1,18 @@
-from flask import Flask, render_template, Response
-import os, requests
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
-import matplotlib.pyplot as plt
-from io import BytesIO
-import sys
-from pathlib import Path
+from flask import Flask, render_template
+import os, requests
 
 
-
-# load_dotenv(dotenv_path=dotenv_path)
+load_dotenv()
 
 app = Flask(__name__, template_folder='templates')
 
-load_dotenv('../.env')
-
-aemet_url = 'https://opendata.aemet.es/opendata'
-
+AEMET_URL = os.getenv('AEMET_URL')
 API_KEY = os.getenv('AEMET_APIKEY')
 
-print(os.environ)
-print('asdf', file=sys.stderr)
-print(API_KEY, file=sys.stderr)
-API_KEY='' # TODO
-
 def get_hourly_predictions_url():
-    url = f"{aemet_url}/api/prediccion/especifica/municipio/horaria/31201?api_key={API_KEY}"
-
+    url = f"{AEMET_URL}/api/prediccion/especifica/municipio/horaria/31201?api_key={API_KEY}"
     payload={}
     headers = {
         'accept': 'application/json'
@@ -38,40 +25,35 @@ def get_hourly_predictions_url():
 
     return None
 
-def get_temperature():
+def get_temperatures():
     response = requests.get(get_hourly_predictions_url())
     if response.status_code == 200:
         temperatures = []
+        dates = []
 
-        for day in response.json()[0]['prediccion']['dia']:
-
+        for day in response.json()[0]['prediccion']['dia']:    
             for temperature in day['temperatura']:
+                dates.append(datetime.strptime(day['fecha'], "%Y-%m-%dT%H:%M:%S") + timedelta(hours=int(temperature['periodo'])))
                 temperatures.append(temperature['value'])
-
-    return [None]
+        
+        return temperatures, dates
+    return [None], [None]
 
 def plot_temperature():
-    temperatures = get_temperature()
-
-    temperatures = [1,2,3,4,5]
-
-    # Create the plot
-    plt.figure(figsize=(10, 5))
-    plt.plot(temperatures, temperatures, marker='o', linestyle='-', color='b', label="Temperature (°C)")
-    plt.xlabel("Time")
-    plt.ylabel("Temperature (°C)")
-    plt.title("Temperature Forecast for the Next 48 Hours")
-    plt.xticks(rotation=45)
-    plt.grid(True)
-    plt.legend()
-
-    # Save the plot to a BytesIO object instead of a file
-    img = BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)  # Rewind the BytesIO object
-    plt.close()
+    import plotly.express as px
+    temperatures, dates = get_temperatures()
     
-    return img
+    if temperatures[0] is None:
+        return '<p>Error fetching data.</p>'
+    
+    fig = px.line(
+        x=dates, 
+        y=temperatures, 
+        labels={'x': '', 'y': 'Temperature (°C)'}
+    )
+    fig.update_layout(xaxis_tickangle=-45)
+    
+    return fig.to_html(full_html=False)
 
 # def get_db_connection():
 #  import mysql.connector
@@ -94,9 +76,8 @@ def plot_temperature():
 
 @app.route("/")
 def home():
-    # return render_template("index.html")
-    img = plot_temperature()
-    return Response(img, mimetype='image/png')
+    plot_html = plot_temperature()
+    return render_template("index.html", plot_html=plot_html)
 
 @app.route("/legal_note")
 def legal_note():
